b00d238d2b8decce1dfdcec2b2a9c8b92a56eda6
CedChris <92729864+CedChris@users.noreply.github.com>
2025-06-26 17:13:16 +0200

    Update MatchList.vue

--- src/components/MatchList.vue
+++ src/components/MatchList.vue
@@ -134,40 +134,6 @@ body {
   padding: 20px;
   overflow-x: hidden; /* Empêche le débordement horizontal */
 }
-.popup-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0,0,0,0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-}
-
-.popup {
-  background: white;
-  padding: 20px 30px;
-  border-radius: 12px;
-  box-shadow: 0 0 10px rgba(0,0,0,0.25);
-  text-align: center;
-}
-
-.popup button {
-  margin-top: 15px;
-  padding: 8px 16px;
-  background-color: #3498db;
-  color: white;
-  border: none;
-  border-radius: 8px;
-  cursor: pointer;
-}
-
-.popup button:hover {
-  background-color: #2980b9;
-}
 /* CONTAINER PRINCIPAL */
 .match-list-container {
   width: 100%;              /* Pleine largeur */
